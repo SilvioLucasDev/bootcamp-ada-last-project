48ba5403ba6b9550107408250affbaa80fde780e
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-03-09 02:15:42 -0300

    test: add tests of DeleteUsersController

---
+++ tests/controllers/users/delete.spec.ts
@@ -0,0 +1,86 @@
+import { DeleteUsersController } from '../../../src/controllers/users/delete'
+import { logger } from '../../mocks/logger'
+import { usersRepositoryMock } from '../../mocks/users_repository'
+import { User, NewUser } from '../../../src/controllers/models'
+import { fakerEN } from '@faker-js/faker'
+import { Request, Response } from 'express'
+
+describe('DeleteBooksController', () => {
+  function makeSut() {
+    const controller = new DeleteUsersController(logger, usersRepositoryMock)
+
+    const newUserMock: NewUser = {
+      name: fakerEN.internet.userName(),
+      email: fakerEN.internet.email(),
+    }
+
+    const userMock: User = {
+      id: fakerEN.string.uuid(),
+      ...newUserMock
+    }
+
+    const requestMock = {
+      body: newUserMock,
+      params: { id: userMock.id } as any
+    } as Request
+
+    const responseMock = {
+      statusCode: 0,
+      status: (status: number) => {
+        responseMock.statusCode = status
+        return {
+          json: jest.fn(),
+          send: jest.fn(),
+        } as any
+      },
+    } as Response
+
+    return {
+      controller, newUserMock, userMock, requestMock, responseMock
+    }
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should delete the user if the user exist', async () => {
+    const { controller, newUserMock, userMock, requestMock, responseMock } = makeSut()
+    jest.spyOn(usersRepositoryMock, 'getById').mockResolvedValueOnce(userMock)
+    jest.spyOn(usersRepositoryMock, 'delete').mockResolvedValueOnce()
+
+    const promise = controller.delete(requestMock, responseMock)
+
+    await expect(promise).resolves.not.toThrow()
+    expect(usersRepositoryMock.getById).toHaveBeenCalledWith(userMock.id)
+    expect(usersRepositoryMock.delete).toHaveBeenCalledWith(userMock.id)
+    expect(usersRepositoryMock.delete).toHaveBeenCalledTimes(1)
+    expect(responseMock.statusCode).toEqual(200)
+  })
+
+  it('should not delete the user if there is no user with the id provided', async () => {
+    const { controller, newUserMock, userMock, requestMock, responseMock } = makeSut()
+    jest.spyOn(usersRepositoryMock, 'getById').mockResolvedValueOnce(undefined)
+    jest.spyOn(usersRepositoryMock, 'delete')
+
+    const promise = controller.delete(requestMock, responseMock)
+
+    await expect(promise).resolves.not.toThrow()
+    expect(usersRepositoryMock.getById).toHaveBeenCalledWith(userMock.id)
+    expect(usersRepositoryMock.delete).not.toHaveBeenCalled()
+    expect(responseMock.statusCode).toEqual(404)
+  })
+
+  it('should return 500 if some error occur', async () => {
+    const { controller, newUserMock, userMock, requestMock, responseMock } = makeSut()
+    jest.spyOn(usersRepositoryMock, 'getById').mockRejectedValueOnce(new Error('some error'))
+    jest.spyOn(usersRepositoryMock, 'delete')
+
+    const promise = controller.delete(requestMock, responseMock)
+
+    await expect(promise).resolves.not.toThrow()
+    expect(usersRepositoryMock.getById).toHaveBeenCalledWith(userMock.id)
+    expect(usersRepositoryMock.delete).not.toHaveBeenCalled()
+    expect(responseMock.statusCode).toEqual(500)
+  })
+})
--- tests/mocks/users_repository.ts
+++ tests/mocks/users_repository.ts
@@ -7,4 +7,5 @@ export const usersRepositoryMock: IUsersRepository = {
   getByEmail: (email: string): Promise<User | undefined> => jest.fn as any,
   list: (): Promise<User[]> => jest.fn as any,
   update: (id: string, book: NewUser): Promise<void> => jest.fn as any,
+  delete: (id: string): Promise<void> => jest.fn as any,
 }
