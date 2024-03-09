6afab2ea5d759cb973a49b40c78b617864790aa6
Enei <eneipereira@outlook.com.br>
2024-03-09 20:42:16 -0300

    feat: add book mocks for integration tests

---
+++ tests/integration/mocks/books.ts
@@ -0,0 +1,46 @@
+import { fakerEN } from '@faker-js/faker';
+import { Book, NewBook } from '../../../src/controllers/models';
+
+export const newBookMock: NewBook = {
+  title: fakerEN.word.words(),
+  subtitle: fakerEN.word.words(),
+  publishing_company: fakerEN.company.name(),
+  published_at: fakerEN.date.anytime(),
+  authors: fakerEN.person.firstName(),
+}
+
+export const bookMock: Book = {
+  id: fakerEN.string.uuid(),
+  title: fakerEN.word.words(),
+  subtitle: fakerEN.word.words(),
+  publishing_company: fakerEN.company.name(),
+  published_at: fakerEN.date.anytime(),
+  authors: fakerEN.person.firstName(),
+}
+
+export const booksMock: Book[] = [
+  {
+    id: fakerEN.string.uuid(),
+    title: fakerEN.word.words(),
+    subtitle: fakerEN.word.words(),
+    publishing_company: fakerEN.company.name(),
+    published_at: fakerEN.date.anytime(),
+    authors: fakerEN.person.firstName(),
+  },
+  {
+    id: fakerEN.string.uuid(),
+    title: fakerEN.word.words(),
+    subtitle: fakerEN.word.words(),
+    publishing_company: fakerEN.company.name(),
+    published_at: fakerEN.date.anytime(),
+    authors: fakerEN.person.firstName(),
+  },
+  {
+    id: fakerEN.string.uuid(),
+    title: fakerEN.word.words(),
+    subtitle: fakerEN.word.words(),
+    publishing_company: fakerEN.company.name(),
+    published_at: fakerEN.date.anytime(),
+    authors: fakerEN.person.firstName(),
+  },
+]
