955b3c7a1e1b07aa70b210556990fcef8261fc7c
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-03-09 00:13:37 -0300

    feat: should return 404 statusCode and not update the user if there is no user with the id provided

--- src/controllers/users/update.ts
+++ src/controllers/users/update.ts
@@ -1,14 +1,24 @@
 import { IUsersRepository } from "../interfaces";
 import { Request, Response } from "express";
 import { Logger } from "winston";
+import { NewUser } from "../models";
 
 export class UpdateUsersController {
   constructor(
     private readonly logger: Logger,
     private readonly usersRepository: IUsersRepository
-  ) {}
+  ) { }
 
-  public async update(req: Request, res: Response): Promise<void> {
-    res.status(501).send({ message: 'not implemented yet!' })
+  public async update(req: Request<any, any, NewUser>, res: Response): Promise<void> {
+    const { id } = req.params
+    const body = req.body
+
+    const user = await this.usersRepository.getById(id)
+    if (!user) {
+      res.status(404).json({ message: 'any user with the id provided was founded' })
+      return
+    }
+
+    await this.usersRepository.update(id, body)
   }
 }
