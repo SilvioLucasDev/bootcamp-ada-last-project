eb7fee7f003e73025576cf8b011b8c75ee7b93dc
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-03-09 00:14:08 -0300

    test: add should return 404 statusCode and not update the user if there is no user with the id provided test in UpdateUsersControllerTest

--- tests/mocks/users_repository.ts
+++ tests/mocks/users_repository.ts
@@ -6,4 +6,5 @@ export const usersRepositoryMock: IUsersRepository = {
   getById: (id: string): Promise<User | undefined> => jest.fn as any,
   getByEmail: (email: string): Promise<User | undefined> => jest.fn as any,
   list: (): Promise<User[]> => jest.fn as any,
+  update: (id: string, book: NewUser): Promise<void> => jest.fn as any,
 }
