fb08be909bc5ed167f79d9979cca2a666fb6f0f8
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-03-09 00:25:39 -0300

    feat: should return 500 if some error occur

--- src/controllers/users/update.ts
+++ src/controllers/users/update.ts
@@ -13,20 +13,29 @@ export class UpdateUsersController {
     const { id } = req.params
     const body = req.body
 
-    const user = await this.usersRepository.getById(id)
-    if (!user) {
-      res.status(404).json({ message: 'any user with the id provided was founded' })
-      return
-    }
-
-    if (body.email) {
-      const withTheSameEmail = await this.usersRepository.getByEmail(body.email)
-      if (withTheSameEmail) {
-        res.status(409).json({ message: 'there is already a user with the same email provided' })
+    try {
+      const user = await this.usersRepository.getById(id)
+      if (!user) {
+        res.status(404).json({ message: 'any user with the id provided was founded' })
         return
       }
-    }
 
-    await this.usersRepository.update(id, body)
+      if (body.email) {
+        const withTheSameEmail = await this.usersRepository.getByEmail(body.email)
+        if (withTheSameEmail) {
+          res.status(409).json({ message: 'there is already a user with the same email provided' })
+          return
+        }
+      }
+
+      await this.usersRepository.update(id, body)
+
+      res.status(200).json({ ...user, ...body, })
+      return
+    } catch (err) {
+      this.logger.error({ message: 'error to update user', error: err })
+      res.status(500).json({ message: 'something went wrong, try again latter!' })
+      return
+    }
   }
 }
